c39d87c23ca5378b8e31e58182e251f793e6030b
Yusri Caloyloy <61104011+Yuserz@users.noreply.github.com>
2022-11-17 12:26:24 +0000

    fix: working backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-# Import the required libraries
 from flask import Flask
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
--- backend/routes.py
+++ backend/routes.py
@@ -1,20 +1,22 @@
-from flask import current_app,jsonify,request
-from app import create_app,db
-from models import Articles,articles_schema
+"""import libraries"""
+from flask import jsonify
+from app import create_app
+from models import Articles, articles_schema
 
 # Create an application instance
 app = create_app()
 
 # Define a route to fetch the avaialable articles
-
+@app.route("/", methods=["GET"], strict_slashes=False)
 @app.route("/articles", methods=["GET"], strict_slashes=False)
+
 def articles():
+    """Function query"""
+    articles = Articles.query.all()
+    results = articles_schema.dump(articles)
 
-	articles = Articles.query.all()
-	results = articles_schema.dump(articles)
-
-	return jsonify(results)
+    return jsonify(results)
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
