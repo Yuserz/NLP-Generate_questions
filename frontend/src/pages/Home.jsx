1c8f6579bb3924fe4ac4022ff5d7b400c1ab7639
Yusri Caloyloy <61104011+Yuserz@users.noreply.github.com>
2022-12-02 09:04:48 +0800

    chore: initiate SignUp

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
     <div className="grid-container w-screen h-screen bg-slate-50">
         <Header />
-        <div className="m-auto flex justify-between mt-5 w-screen ">
+        <div className="m-auto flex justify-between p-5">
             <Navbar />
             <div className=" w-screen p-10 bg-amber-200 rounded-lg">
                 <ContextForm />
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,99 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import swal from 'sweetalert';
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: ""
+}
 
 export default function SignUp() {
+  const [
+    { firstName, lastName, email, password },
+    setState,
+  ] = useState(initialState)
+
+  const navigate = useNavigate();
+  const [error, setError] = useState()
+
+  const onChange = (event) => {
+    const { name, value } = event.target
+    setState((prevState) => ({ ...prevState, [name]: value }))
+  }
+
+  // const onSubmit = async (event) => {
+  //   event.preventDefault()
+
+  //   if (password !== confirm_password) {
+  //     setError('Password does not match!')
+  //   }
+
+  //   if (error) {
+  //     swal.fire({
+  //       title: error,
+  //       icon: "error",
+  //     }).then((result) => {
+  //       if (result.isConfirmed) {
+  //         return
+  //       }
+  //     })
+  //   }
+  //   else {
+  //     try {
+  //       const datas = {
+  //         firstName,
+  //         lastName,
+  //         email,
+  //         userType,
+  //         password,
+  //       }
+
+  //       const response = await axiosRequest.post("/api/v1/signup", datas)
+
+  //       const { status } = response
+
+  //       if (status === 201) {
+  //         swal.fire({
+  //           title: "Successfully Signup",
+  //           text: "click ok to continue",
+  //           icon: "success",
+  //         }).then((result) => {
+  //           if (result.isConfirmed) {
+  //             navigate('/')
+  //           }
+  //         })
+  //       }
+  //     } catch (error) {
+  //       const { status } = error.response
+
+  //       if (status === 500) {
+  //         swal.fire({
+  //           title: "Oops!! Error 500",
+  //           text: "server not found",
+  //           icon: "warning",
+  //         })
+  //       }
+
+  //       if (status === 404) {
+  //         swal.fire({
+  //           title: "Oops!!",
+  //           text: "something went wrong, please try again :(",
+  //           icon: "warning",
+  //         })
+  //       }
+
+  //       if (status === 409) {
+  //         swal.fire({
+  //           title: "Error",
+  //           text: "Email already taken!",
+  //           icon: "warning",
+  //         })
+  //       }
+  //     }
+  //   }
+  // }
 
   return (
     <div className="flex items-center justify-evenly m-auto pl-5 pr-5 max-w-[1240px] h-screen">
@@ -7,18 +101,21 @@ export default function SignUp() {
       <div className="flex items-center justify-center w-[50%] h-[80%] p-10 bg-amber-200 rounded-xl">
         <div className="flex flex-col max-w-[400px] w-full space-y-5 ml-10 mr-10 items-center ">
           <p className="text-4xl font-medium mb-10">Study Budy</p>
+          <div className='flex gap-5'>
+            <input className="rounded-md p-2 w-full shadow-inner bg-white outline-gray-300 max-w-[400px]" placeholder="First Name" type="Email" />
+            <input className="rounded-md p-2 w-full shadow-inner bg-white outline-gray-300  max-w-[400px]" placeholder="Last Name" type="password" />
+          </div>
           <input className="rounded-md p-2 w-full shadow-inner bg-white outline-gray-300 max-w-[400px]" placeholder="Email" type="Email" />
           <input className="rounded-md p-2 w-full shadow-inner bg-white outline-gray-300  max-w-[400px]" placeholder="Password" type="password" />
-          {/* <div className="flex items-center gap-2 w-full max-w-[400px] p-1">
-            <input type="checkbox" id="fname" name="fname" />
-            <label for="fname" className="opacity-80">Remember me</label>
-          </div> */}
-          <button className="bg-white max-w-[400px] w-full rounded-md p-4 shadow-md hover:shadow-xl font-bold text-xl" type="submit">SignUp</button>
+          <button className="bg-white max-w-[400px] w-full rounded-md p-4 shadow-md hover:shadow-xl font-bold text-l" type="submit">Sign Up</button>
           <div className="w-full flex justify-end p-1 opacity-70">
+            <p>Already have an account?</p>
+          </div>
+          <div>
+            <button type="submit"></button>
           </div>
         </div>
       </div>
-
     </div>   
   )
 }
