ba433a4ba5e90dfa6667777dda057544fbc443d9
xShiro2 <daryllejumalon16@gmail.com>
2022-12-06 10:19:18 +0800

    chore!: implement flask migrate

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisakey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -14,17 +18,17 @@ login_manager.init_app(app)
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
 
-from app import routes
-from app import models
+from app import Routes
+from app import Models
 from app.T5 import t5model
-t5model.initialize()
+#t5model.initialize()
 
-@app.before_first_request
-def create_tables():
-    #db.drop_all()
-    db.create_all()
-    db.session.commit()
+# @app.before_first_request
+# def create_tables():
+#     #db.drop_all()
+#     db.create_all()
+#     db.session.commit()
 
 @login_manager.user_loader
 def load_user(user_id):
-    return models.User.query.get(int(user_id))
+    return Models.user.User.query.get(int(user_id))
