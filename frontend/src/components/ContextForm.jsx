4a6f29fa1cb46ec1cc8a211eb81a952321d163d5
xShiro2 <daryllejumalon16@gmail.com>
2022-12-06 13:08:19 +0800

    feat: add questionare

--- frontend/src/components/ContextForm.jsx
+++ frontend/src/components/ContextForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { axiosRequest } from "api"
+import { Test } from 'components'
 
 const initialState = {
     context: "",
@@ -8,7 +9,7 @@ const initialState = {
 }
 
 export default function ContextForm() {
-    const [questions, setQuestions] = useState([])
+    const [questions, setQuestions] = useState("")
     const [{ context, topic, subject }, setState] = useState(initialState)
 
     const url = "/generate"
@@ -22,7 +23,6 @@ export default function ContextForm() {
 
             if (status === 200) {
                 setQuestions(data.data)
-                console.log(data.data)
             }
 
         } catch {
@@ -34,42 +34,21 @@ export default function ContextForm() {
         const { name, value } = event.target
         setState((prevState) => ({ ...prevState, [name]: value }))
     }
-    
-    const getLetter = (index) => {
-        const letters = ['A', 'B', 'C', 'D']
-        return letters[index]
-    }
 
     return (
-        <div className="flex flex-col gap-y-10">
-            <div className=" flex flex-col space-y-5 items-center">
-                <div className="flex space-x-4 w-[100%]">
-                    <textarea name="course" value={subject} onChange={(event) => onChange(event)} cols="20" rows="1" placeholder="Subject" className="outline-none  resize-none p-2 rounded-lg"></textarea>
-                    <textarea name="subject" value={topic} onChange={(event) => onChange(event)} cols="20" rows="1" placeholder="Topic" className="outline-none resize-none p-2 rounded-lg"></textarea>
+        <div className="flex flex-col gap-y-10 items-center">
+            {questions ?
+                <Test questions={questions} context={context}/> :
+
+                <div className=" flex flex-col space-y-5 items-center">
+                    <div className="flex space-x-4 w-[100%]">
+                        <textarea name="subject" value={subject} onChange={(event) => onChange(event)} cols="20" rows="1" placeholder="Subject" className="outline-none  resize-none p-2 rounded-lg"></textarea>
+                        <textarea name="topic" value={topic} onChange={(event) => onChange(event)} cols="20" rows="1" placeholder="Topic" className="outline-none resize-none p-2 rounded-lg"></textarea>
+                    </div>
+                    <textarea name="context" onChange={(event) => onChange(event)} value={context} rows="18" className="outline-none rounded-lg block resize-none p-2.5 w-full  text-sm " placeholder="Input your context here..."></textarea>
+                    <button onClick={generate} className="p-4 bg-white w-fit rounded-lg">Generate Question</button>
                 </div>
-                <textarea name="context" onChange={(event) => onChange(event)} value={context} rows="18" className="outline-none rounded-lg block resize-none p-2.5 w-full  text-sm " placeholder="Input your context here..."></textarea>
-                <button onClick={generate} className="p-4 bg-white w-fit rounded-lg">Generate Question</button>
-            </div>
-            <div className="flex flex-col gap-y-3 w-fit h-fit">
-                {questions ? questions.map((question, index) => {
-                    return (
-                        <div key={index} className="flex flex-col gap-y-1">
-                            <p>Question: {question.question}</p>
-                            
-                            <p>Multiple Choice:</p>
-                            <div className="flex flex-col ml-2">
-                                
-                                {question.choices.map((choice, index) => {
-                                    return <p key={index}>{getLetter(index)}. {choice}</p>
-                                })}
-                            </div>
-
-                            <p>Correct Answer: {question.answer}</p>
-                        </div>
-                    )
-                }) : null}
-            </div>
-
+            }
         </div>
     )
 }
---
+++ frontend/src/components/Test.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+import { Check, X } from 'react-feather'
+
+export default function Test({ context, questions }) {
+    const [selected, setSelected] = useState([])
+    const [correct, setCorrect] = useState([])
+    const [isSubmit, setSubmit] = useState(false)
+    const [showContext, setShowContext] = useState(false)
+
+    const getLetter = (index) => {
+        const letters = ['A', 'B', 'C', 'D']
+        return letters[index]
+    }
+
+    const submit = () => {
+        setSubmit(prevState => !prevState)
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setSelected((prevState) => ({ ...prevState, [name]: value }))
+    }
+
+    const handleCorrect = (event, answer, choice) => {
+        const { name } = event.target
+        setCorrect((prevState) => ({ ...prevState, [name]: answer === choice ? true : false }))
+    }
+
+    const show = () => {
+        setShowContext(prevState => !prevState)
+    }
+
+    return (
+        <div className="flex flex-col gap-y-5 items-center">
+            {showContext ?
+                <div className="bg-white p-5 rounded-lg">
+                    {context}
+                </div>
+                : null
+            }
+            <button className="rounded border border-black w-40 p-3" onClick={show}>{showContext ? 'Hide' : 'Show'} Context</button>
+            <div className="flex flex-col gap-y-3 w-fit h-fit bg-white rounded-lg p-10">
+                {questions.map((question, index) => {
+                    return (
+                        <div key={index} className="flex flex-col gap-y-5">
+                            <p className="font-medium">{index + 1}. Question: {question.question}</p>
+                            <div className="flex flex-col ml-2 items-start gap-y-2">
+                                {question.choices.map((choice, i) => {
+                                    return (
+                                        <div key={i} className="w-full flex flex-row">
+                                            <input id={`c-${index}-${i}`}
+                                                type='radio'
+                                                name={index}
+                                                value={choice}
+                                                className="hidden peer"
+                                                onChange={e => { handleChange(e); handleCorrect(e, question.answer, choice) }}
+                                                disabled={isSubmit}
+                                            />
+
+                                            <label for={`c-${index}-${i}`}
+                                                className={`rounded border flex flex-row justify-between  items-center w-full
+                                                ${selected[index] === choice && !isSubmit ? 'bg-blue-100' : ""}
+                                                ${correct[index] && selected[index] === choice && isSubmit ? 'bg-green-100' : ''}
+                                                ${!correct[index] && selected[index] === choice && isSubmit ? 'bg-red-100' : ''}
+                                                ${!correct[index] && question.answer === choice && isSubmit ? 'bg-green-100' : ''}
+                                                `}
+                                            >
+                                                <div className="flex flex-row gap-x-3">
+                                                    <p className={`bg-gray-100 py-2 px-5 font-medium
+                                                    ${selected[index] === choice && !isSubmit ? 'bg-blue-300' : ""}
+                                                    ${correct[index] && selected[index] === choice && isSubmit ? 'bg-green-300' : ''}
+                                                    ${!correct[index] && selected[index] === choice && isSubmit ? 'bg-red-300' : ''}
+                                                    ${!correct[index] && question.answer === choice && isSubmit ? 'bg-green-300' : ''}
+                                                    `}
+                                                    >
+                                                        {getLetter(i)}</p>
+                                                    <p className="py-2 ">{choice}</p>
+                                                </div>
+                                                <div className="p-2">
+                                                    {isSubmit && selected[index] === choice ?
+                                                        correct[index] ? <Check className='w-5' /> : <X className='w-5' />
+                                                        :
+                                                        null
+                                                    }
+                                                </div>
+                                            </label>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+            <button className="rounded border border-black w-40 p-3" onClick={submit}>SUBMIT ANSWER</button>
+        </div>
+    )
+}
