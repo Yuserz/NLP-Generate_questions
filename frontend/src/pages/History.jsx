0dc4726612881f7c66fff5a628c71f91d859d67f
xShiro2 <daryllejumalon16@gmail.com>
2022-12-13 11:20:07 +0800

    fix: style when history is empty

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -1,35 +1,35 @@
-import {  Header, Navbar, Table } from "components"
+import { Header, Navbar, Table } from "components"
 import { useState, useEffect } from "react"
 import { axiosRequest } from "api"
 
 export default function History() {
-      const url = "/history"
+    const url = "/history"
 
-      const [ data, setData ] = useState(0);
-      useEffect(() => {
-        const showData = async() => {
+    const [data, setData] = useState(0);
+    useEffect(() => {
+        const showData = async () => {
             const response = await axiosRequest.get(url)
             const { status, data } = response
 
-            if(status === 200){
+            if (status === 200) {
                 setData(data.data)
             }
         }
         showData();
-      }, []);
+    }, []);
 
 
     return (
         <div className="grid-container bg-slate-50">
-        <Header />
-        {data ? 
-        <div className="m-auto h-full flex justify-between">
-            <Navbar className="bg-amber-200 font-bold" />
-            <div className="flex flex-col gap-5 items-center w-screen p-10 bg-amber-200">
-                <Table data={data} />
-            </div>
+            <Header />
+            {data ?
+                <div className="m-auto h-full min-h-screen flex justify-between">
+                    <Navbar className="bg-amber-200 font-bold" />
+                    <div className="flex flex-col gap-5 items-center w-screen p-10 bg-amber-200">
+                        <Table data={data} />
+                    </div>
+                </div>
+                : null}
         </div>
-        : null}
-    </div> 
     )
 }
