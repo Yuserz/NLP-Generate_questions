5d912b5de2186072e1bc1294eda97805d1318994
Yusri Caloyloy <61104011+Yuserz@users.noreply.github.com>
2022-12-14 11:07:25 +0800

    chore: bump

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,11 @@ import { ContextForm, Header,  Navbar } from "components"
 
 export default function Home() {
     return (
-    <div className="grid-container bg-slate-50">
+    <div className="grid-container overflow-hidden bg-slate-50">
         <Header />
-        <div className="flex justify-between">
+        <div className="h-full flex justify-between">
             <Navbar />
-            <div className="w-screen h-screen p-10 bg-amber-200 ">
+            <div className="w-full p-10 bg-amber-200 ">
                 <ContextForm />
             </div>
        </div>
